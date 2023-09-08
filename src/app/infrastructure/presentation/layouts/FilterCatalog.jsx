c0169b85636fa11e7102d3ff2a25673cdc5bc47a
Brandol Fontecha <brandol.fontecha@moviired.co>
2023-09-08 09:38:44 -0500

    min height nav filter

--- src/app/infrastructure/presentation/layouts/FilterCatalog.jsx
+++ src/app/infrastructure/presentation/layouts/FilterCatalog.jsx
@@ -9,62 +9,64 @@ function FilterCatalog({ linkWhatsApp, linkFacebook }) {
   const { isFixed } = useIsFixedScroll({ elementById: fixedElement });
 
   return (
-    <section
-      id={fixedElement}
-      className={`${
-        isFixed
-          ? "fixed top-0 left-0 rounded-b-md py-3 px-3 xl:px-28 animate-opacity transition-opacity duration-700 opacity-1"
-          : "relative rounded-3xl py-3 px-4 xl:px-12 xl:py-5 animate-opacity transition-opacity duration-700 opacity-1"
-      } 
-        border-solid border-[1px] border-[rgb(0,0,0,.1)] w-full [transition:_all_0.5s_ease] mb-20 shadow-md z-20 bg-white`}
-    >
-      <div className="flex flex-row justify-around">
-        <div className="flex flex-col gap-3">
-          <p className="text-slate-800 text-center font-semibold text-sm">
-            Pagos
-          </p>
-          <div className="border-solid border-b-[1px] border-indigo-200 rounded-lg"></div>
-          <img
-            src={NequiLogo}
-            className="w-24"
-            alt="Pagos por Nequi"
-            width="200"
-            height="80"
-          />
-        </div>
+    <section className="min-h-[4rem] xl:min-h-[7rem] w-full">
+      <article
+        id={fixedElement}
+        className={`${
+          isFixed
+            ? "fixed top-0 left-0 rounded-b-md py-3 px-3 xl:px-28 animate-flip-down animate-duration-700 animate-ease-in-out"
+            : "relative rounded-3xl py-3 px-4 xl:px-12 xl:py-5 animate-flip-down animate-duration-700 animate-ease-in-out"
+        } 
+          border-solid border-[1px] border-[rgb(0,0,0,.1)] w-full [transition:_all_0.5s_ease] mb-20 shadow-md z-20 bg-white`}
+      >
+        <div className="flex flex-row justify-around">
+          <div className="flex flex-col gap-3">
+            <p className="text-slate-800 text-center font-semibold text-sm">
+              Pagos
+            </p>
+            <div className="border-solid border-b-[1px] border-indigo-200 rounded-lg"></div>
+            <img
+              src={NequiLogo}
+              className="w-24"
+              alt="Pagos por Nequi"
+              width="200"
+              height="80"
+            />
+          </div>
 
-        <div className="flex flex-col gap-3">
-          <p className="text-slate-800 text-center font-semibold text-sm">
-            Contactanos
-          </p>
-          <div className="border-solid border-b-[1px] border-indigo-200 rounded-lg"></div>
-          <div className="grid grid-cols-2 gap-2 xl:gap-6">
-            <a
-              href={linkWhatsApp}
-              aria-label="Consultar más sobre nuestros procutos por WhatsApp"
-              className="cursor-pointer hover:scale-110 transition-all delay-75"
-            >
-              <IconWhatsApp
-                classProp={`${
-                  isFixed ? "w-8 h-8" : "w-10 h-10"
-                } text-[#25D366] drop-shadow-md animate-jump-in`}
-              ></IconWhatsApp>
-            </a>
-            <a
-              href={linkFacebook}
-              aria-label="Consultar más sobre nuestros procutos por Facebook"
-              className="cursor-pointer hover:scale-110 transition-all delay-75"
-            >
-              <IconFacebook
-                classProp={`${
-                  isFixed ? "w-8 h-8" : "w-10 h-10"
-                } text-[#4267B2] drop-shadow-md animate-jump-in`}
-              ></IconFacebook>
-            </a>
+          <div className="flex flex-col gap-3">
+            <p className="text-slate-800 text-center font-semibold text-sm">
+              Contactanos
+            </p>
+            <div className="border-solid border-b-[1px] border-indigo-200 rounded-lg"></div>
+            <div className="grid grid-cols-2 gap-2 xl:gap-6">
+              <a
+                href={linkWhatsApp}
+                aria-label="Consultar más sobre nuestros procutos por WhatsApp"
+                className="cursor-pointer hover:scale-110 transition-all delay-75"
+              >
+                <IconWhatsApp
+                  classProp={`${
+                    isFixed ? "w-8 h-8" : "w-10 h-10"
+                  } text-[#25D366] drop-shadow-md animate-jump-in`}
+                ></IconWhatsApp>
+              </a>
+              <a
+                href={linkFacebook}
+                aria-label="Consultar más sobre nuestros procutos por Facebook"
+                className="cursor-pointer hover:scale-110 transition-all delay-75"
+              >
+                <IconFacebook
+                  classProp={`${
+                    isFixed ? "w-8 h-8" : "w-10 h-10"
+                  } text-[#4267B2] drop-shadow-md animate-jump-in`}
+                ></IconFacebook>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div id="scroll-top--fixed"></div>
+      </article>
+
     </section>
   );
 }
